16188872756ceb0c6fb40f713b428db4d99530c5
Logan Thomas <Lathomas42@gmail.com>
2014-08-02 18:02:26 -0400

    CameraFollowScript

---
+++ GBJAM/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 camPos = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
+		Vector3 playPos = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z);
+		if(Vector3.Distance (camPos,playPos)>=.3){
+			this.transform.position = Vector3.Slerp (camPos, playPos, Time.deltaTime*2);
+		}
+	}
+}
