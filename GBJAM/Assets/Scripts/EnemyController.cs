3aee0752335627d7f216b0447b69c1c1171b9b9c
CollinHern <collinhern@gmail.com>
2014-08-02 18:57:58 -0400

    Merge remote-tracking branch 'origin/master'

---
+++ GBJAM/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+	public GameObject platform;
+	public GameObject target;
+	public int right = 1;
+	public float enemyDt = 1f;
+	public CharacterController enemyControl;
+	public Vector3 moveDir = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.move ();
+	}
+
+	void patrolPlatform (){
+		if (this.right == 1 && this.transform.position.x <= this.platform.collider.bounds.max.x-this.enemyControl.radius) {
+			this.moveDir = new Vector3 (1f,0,0);
+		}
+		else if (this.right == -1 && this.transform.position.x >= this.platform.collider.bounds.min.x+this.enemyControl.radius) {
+			this.moveDir = new Vector3 (-1f,0,0);
+		}
+		else {
+			this.right *= -1;
+		}
+
+	}
+
+	void move(){
+		enemyControl = GetComponent<CharacterController>();
+		if (Vector3.Distance (this.transform.position, this.target.transform.position) >= .5) {
+				this.patrolPlatform ();
+		}
+		else{
+			this.moveDir = Vector3.zero;
+		}
+		this.transform.localScale =new Vector3 (this.right, 1f, 1f);
+		this.enemyControl.Move (moveDir * enemyDt * Time.deltaTime);
+
+	}
+}
