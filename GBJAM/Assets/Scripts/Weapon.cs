8658dfe289377833dd6f75dedfd8d35de89f9f89
CollinHern <collinhern@gmail.com>
2014-08-02 01:11:59 -0400

    Test weapon

---
+++ GBJAM/Assets/Scripts/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnTriggerEnter()
+    {
+        Destroy(gameObject);
+        System.Console.WriteLine("bam");
+    }
+
+
+}
