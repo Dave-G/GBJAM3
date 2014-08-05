47b97d261286536cb0dec279068d0096e01f95a4
Logan Thomas <Lathomas42@gmail.com>
2014-08-05 19:09:27 -0400

    Camera, Now with bounds!

--- GBJAM/Assets/Scripts/CameraFollow.cs
+++ GBJAM/Assets/Scripts/CameraFollow.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 	public GameObject player;
+	public GameObject Background;
 
 
 	// Use this for initialization
@@ -15,8 +16,10 @@ public class CameraFollow : MonoBehaviour {
 	void Update () {
         Screen.SetResolution(160, 144, false);
 		Vector3 camPos = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z);
-		Vector3 playPos = new Vector3 (player.transform.position.x, player.transform.position.y, this.transform.position.z);
-		if(Vector3.Distance (camPos,playPos)>=.3){
+		Vector3 playPos = new Vector3 (this.player.transform.position.x, this.player.transform.position.y, this.transform.position.z);
+		playPos.x = Mathf.Clamp (this.player.transform.position.x, this.Background.renderer.bounds.min.x+1.6f/2f,this.Background.renderer.bounds.max.x-1.6f/2f);
+		playPos.y = Mathf.Clamp (this.player.transform.position.y, this.Background.renderer.bounds.min.y+1.44f/2f,this.Background.renderer.bounds.max.y-1.44f/2f);
+		if((Vector3.Distance (camPos,playPos)>=.3)||(player.transform.position.x != playPos.x)||(playPos.y != player.transform.position.y)){
 			this.transform.position = Vector3.Slerp (camPos, playPos, Time.deltaTime*2);
 		}
 	}
