d64d1f50e8364d1c395d6097fe4c81ec96c1f0d4
Logan Thomas <Lathomas42@gmail.com>
2014-08-09 19:57:15 -0400

    Bulk Update

--- GBJAM/Assets/Scripts/Boulderhaviour.cs
+++ GBJAM/Assets/Scripts/Boulderhaviour.cs
@@ -2,20 +2,39 @@
 using System.Collections;
 
 public class Boulderhaviour : MonoBehaviour {
-
-	// Use this for initialization
+	private float gravity;
+	private float myDt=1;
+	public int done = 1;
+	public float vel;
+	// Use this for initialization\
+	public Vector3 direction;
 	void Start () {
 	
 	}
 
-	public void setup(Vector3 Dir, float Power, GameObject owner){
+	public void setup(Vector3 Dir, float Power,float gravity, GameObject owner){
+		this.direction = Dir;
+		this.gravity = gravity;
 		this.transform.position = owner.transform.position;
-		this.rigidbody.velocity =  (Dir*Power);
-		Debug.Log(this.rigidbody.velocity);
+		this.vel =  Power;
+		this.rigidbody.velocity = (Dir*Power*myDt*done);
+		Debug.Log (Dir*Power*myDt*done);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		this.myDt = this.GetComponent<BubActivator> ().getDT ();
+		if(this.done != 0){
+			this.rigidbody.velocity = new Vector3(this.direction.x * this.vel * this.myDt,this.myDt*this.rigidbody.velocity.y,0);
+		}
+		this.rigidbody.velocity -= new Vector3(0,this.myDt* this.gravity *Time.fixedDeltaTime,0);
+		Debug.Log (rigidbody.velocity);
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.collider.gameObject.layer == 11 || collision.collider.gameObject.layer == 9){
+
+			this.done = 0;
+		}
 	}
 }
--- GBJAM/Assets/Scripts/TheBulkRoll.cs
+++ GBJAM/Assets/Scripts/TheBulkRoll.cs
@@ -6,6 +6,7 @@ public class TheBulkRoll : MonoBehaviour {
 	public GameObject target;
 	public GameObject brock;
 	public float LastToss = -1;
+	public float throwGrav = 5f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,7 +15,8 @@ public class TheBulkRoll : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Time.time - this.LastToss > 2f){
-			if(Mathf.Abs(this.transform.position.y - target.transform.position.y)<.3 && Mathf.Abs (this.transform.position.x - target.transform.position.x)<1f){
+			if(Mathf.Abs(this.transform.position.y - target.transform.position.y)<.1 && Mathf.Abs (this.transform.position.x - target.transform.position.x)<1f){
+				Debug.Log ("rollin");
 				rollRock();
 			}
 			else{
@@ -26,14 +28,13 @@ public class TheBulkRoll : MonoBehaviour {
 	void rockToss(){
 		this.LastToss = Time.time;
 		float Delx = -1*(target.transform.position.x - this.transform.position.x);
-		float calc = Delx * 9.8f / (Mathf.Pow (ThrowPower,2));
+		float calc = Delx *throwGrav / (Mathf.Pow (ThrowPower,2));
 		float Theta = Mathf.Asin (calc)/2f;
-		Debug.Log (Theta);
 		GameObject rockInstance = (GameObject) Instantiate(brock,this.transform.position,Quaternion.Euler(new Vector3(0, 0, 0)));
-		if(Mathf.Abs(Theta) <= .15){
+		if(Mathf.Abs(Theta) <= .2){
 			Theta = Mathf.PI/2 - Theta;
 		}
-		rockInstance.GetComponent<Boulderhaviour>().setup(new Vector3(-1*Mathf.Cos (Theta),Mathf.Sin (Theta),0),ThrowPower,this.gameObject);
+		rockInstance.GetComponent<Boulderhaviour>().setup(new Vector3(-1*Mathf.Cos (Theta),Mathf.Sin (Theta),0),ThrowPower,throwGrav,this.gameObject);
 		Destroy(rockInstance,3f);
 		//rockInstance.GetComponent<Boulderhaviour>().setup(new Vector3(-1,1,0),ThrowPower,this.gameObject);
 	}
@@ -42,7 +43,7 @@ public class TheBulkRoll : MonoBehaviour {
 		this.LastToss = Time.time;
 		float Delx = this.transform.position.x-target.transform.position.x;
 		GameObject rockInstance = (GameObject) Instantiate (brock,this.transform.position,Quaternion.Euler (new Vector3(0,0,0)));
-		rockInstance.GetComponent<Boulderhaviour>().setup (new Vector3(-1*Mathf.Sign(Delx),0,0),ThrowPower/2f,this.gameObject);
+		rockInstance.GetComponent<Boulderhaviour>().setup (new Vector3(-1*Mathf.Sign(Delx),0,0),ThrowPower/2f,throwGrav,this.gameObject);
 		Destroy(rockInstance,3f);
 	}
 }
