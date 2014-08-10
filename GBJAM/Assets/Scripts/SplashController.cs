f791c129ff11c9cdc2b2ee017ebced3bdda6b52b
Dave Gedarovich <gedarovich@hotmail.com>
2014-08-09 22:03:47 -0400

    Title/Splash keys and procedural axes

--- GBJAM/Assets/Scripts/SplashController.cs
+++ GBJAM/Assets/Scripts/SplashController.cs
@@ -4,18 +4,31 @@ using System.Collections;
 public class SplashController : MonoBehaviour {
 
 	private float timer;
+	private bool goingUp;
+	public GameObject axe;
 	
 	// Use this for initialization
 	void Start () {
 		timer = Time.time;
 		Time.timeScale = 1f;
+		this.createAxes(50);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time > timer + 1f) {	
+		if (Time.time > timer + 3f) {	
 			Application.LoadLevel("Title");
-		}		
+		}
 	}
+
+	// Procedurally drop axes
+	void createAxes(int num){
+		for (int i=0;i<num;i++){
+			Vector3 axePos = new Vector3(this.transform.position.x+Random.Range(-1f,1f),this.transform.position.y+Random.Range(1f,2.5f),this.transform.position.z);
+			GameObject axeClone = (GameObject) Instantiate(this.axe, axePos, this.transform.rotation);
+			axeClone.transform.Rotate(0, 0, Random.Range(0, 360f));
+		}
+	}
+	
 
 }
